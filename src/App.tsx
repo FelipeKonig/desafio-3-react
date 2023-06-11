c375e5000d4d56c00034eb28b7de89182aac1155
felipe konig <lipekonig@gmail.com>
2023-06-10 21:10:40 -0300

    default theme and global style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './layouts/defaultLayout/global'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
